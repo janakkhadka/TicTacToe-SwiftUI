569b4748faf22cb57825cb06350d5084c62a65ff
Janak Khadka <janakkhadka08@gmail.com>
2025-03-13 23:19:04 +0545

    firebase configured and initialize

--- Tic Tac Toe/Tic Tac Toe/Tic_Tac_ToeApp.swift
+++ Tic Tac Toe/Tic Tac Toe/Tic_Tac_ToeApp.swift
@@ -6,12 +6,30 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct Tic_Tac_ToeApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             FirstView()
         }
     }
 }
+
+//to initialize firebase
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        print("firebase configured!")
+        return true
+        
+    }
+    
+}
+
+
+
