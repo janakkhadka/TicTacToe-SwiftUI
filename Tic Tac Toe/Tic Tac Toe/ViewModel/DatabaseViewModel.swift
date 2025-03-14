2f1bcdd74ee6102d81357f7c3897c37cfc5731e6
Janak Khadka <janakkhadka08@gmail.com>
2025-03-14 23:56:27 +0545

    viewmodel start

---
+++ Tic Tac Toe/Tic Tac Toe/ViewModel/DatabaseViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  ViewModel.swift
+//  Tic Tac Toe
+//
+//  Created by Janak Khadka on 14/03/2025.
+//
+
+import Foundation
+import FirebaseDatabase
+
+class DatabaseViewModel: ObservableObject {
+    let dbRef = Database.database(url: "https://tic-tac-toe-f606f-default-rtdb.asia-southeast1.firebasedatabase.app").reference()
+    
+    
+}
--- Tic Tac Toe/Tic Tac Toe/ViewModel/ViewModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ViewModel.swift
-//  Tic Tac Toe
-//
-//  Created by Janak Khadka on 14/03/2025.
-//
-
-
