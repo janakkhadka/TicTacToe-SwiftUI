ea46497d65bfae9dd2942f7afeb157798343638e
Janak Khadka <janakkhadka08@gmail.com>
2025-03-15 23:50:22 +0545

    model update

--- Tic Tac Toe/Tic Tac Toe/Model/Board.swift
+++ Tic Tac Toe/Tic Tac Toe/Model/Board.swift
@@ -6,11 +6,15 @@
 //
 import Foundation
 
-struct GameBoard: Identifiable, Codable { //codeable le chai json haru maa change garna help garxa swift obj lai, identifiable le chai unique idendity dinxa each element lai
+struct GameBoard: Codable { //codeable le chai json haru maa change garna help garxa swift obj lai, identifiable le chai unique idendity dinxa each element lai
     var board: [String]
+    var isXTurn: Bool
+    var winner: String?
     
     init() {
         self.board = Array(repeating: "", count: 9)
+        self.isXTurn = true
+        self.winner = nil
     }
     
 }
