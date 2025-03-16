6fa1ea24d6bcca8362022e0f88d7aeb4ceb20864
Janak Khadka <janakkhadka08@gmail.com>
2025-03-16 23:42:59 +0545

    sending game data to firebase function

--- Tic Tac Toe/Tic Tac Toe/Model/Board.swift
+++ Tic Tac Toe/Tic Tac Toe/Model/Board.swift
@@ -7,14 +7,14 @@
 import Foundation
 
 struct GameBoard: Codable { //codeable le chai json haru maa change garna help garxa swift obj lai, identifiable le chai unique idendity dinxa each element lai
-    var board: [String]
+    var boardValue: [String]
     var isXTurn: Bool
     var winner: String?
     var xWinningCount: Int
     var oWinningCount: Int
     
     init() {
-        self.board = Array(repeating: "", count: 9)
+        self.boardValue = Array(repeating: "", count: 9)
         self.isXTurn = true
         self.winner = nil
         self.xWinningCount = 0
---
+++ Tic Tac Toe/Tic Tac Toe/ViewModel/GameViewModel.swift
@@ -0,0 +1,31 @@
+//
+//  ViewModel.swift
+//  Tic Tac Toe
+//
+//  Created by Janak Khadka on 14/03/2025.
+//
+
+import Foundation
+import FirebaseDatabase
+
+class GameViewModel: ObservableObject {
+    let dbRef = Database.database(url: "https://tic-tac-toe-f606f-default-rtdb.asia-southeast1.firebasedatabase.app").reference()
+    
+    @Published var board = GameBoard()
+    
+    //value database maa pathauna lai
+    func sendGameData() {
+        do {
+            let data = try JSONEncoder().encode(board)
+            if let json = try JSONSerialization.jsonObject(with: data) as? [String: Any] {
+                dbRef.child(UUID().uuidString).setValue(json)
+            }
+        } catch {
+            print("error encoding")
+        }
+    }
+    
+    
+    
+    
+}
