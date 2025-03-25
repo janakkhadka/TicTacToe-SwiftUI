f1fe00ae80cf835be279faed2a5abc713f595a64
Janak Khadka <janakkhadka08@gmail.com>
2025-03-25 23:50:42 +0545

    fake

--- Tic Tac Toe/Tic Tac Toe/View/FirstView.swift
+++ Tic Tac Toe/Tic Tac Toe/View/FirstView.swift
@@ -52,8 +52,7 @@ struct FirstView: View {
                 Button("Save"){
                     save()
                 }
-            }
-            
+            }//
         }
     }
     func save(){
